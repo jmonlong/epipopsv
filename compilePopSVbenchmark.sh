e3a34f91a9e87420ce3adc296ae01c6470ee7a47
jmonlong <jean.monlong@mail.mcgill.ca>
2016-12-02 19:41:43 -0500

    update reports

--- compilePopSVbenchmark.sh
+++ compilePopSVbenchmark.sh
@@ -1,6 +1,7 @@
 for rmd in src/PopSV*.Rmd src/*wgs-bias.Rmd
 do
     Rscript -e "rmarkdown::render('$rmd')"
-    mv -f ${rmd%Rmd}md reports/
-    mv  -f ${rmd%.Rmd}_files reports/
+    mv ${rmd%Rmd}md reports/
+    cp -r ${rmd%.Rmd}_files reports/
+    rm -rf ${rmd%.Rmd}_files
 done
